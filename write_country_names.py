47d18ac4f5fbb31cdcad81dd5d22287245828aff
salty-stalin <53297144+salty-stalin@users.noreply.github.com>
2020-04-07 23:56:50 +0100

    Update write_country_names.py

--- write_country_names.py
+++ write_country_names.py
@@ -3,16 +3,8 @@ import os
 a = open("country_names.txt", "w")
 for path, subdirs, files in os.walk(r'Countries File'):
    for filename in files:
-      a.write(filename + os.linesep) 
+      filename_new= filename.replace(' ', '_')
+      a.write(filename_new + os.linesep) 
+      os.rename("Countries File\\"+str(filename),"Countries File\\"+str(filename_new))
 a.close()
 
-b=open("country_names.txt","r")
-
-
-Lines = b.readlines() 
-c=open("country_names.txt","w")
-
-# Strips the newline character 
-for line in Lines: 
-    line = line.replace(' ', '_')
-    c.write(line)
