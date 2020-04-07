6b9b464a183da62dc5a327ffc2b2200a39ff2006
salty-stalin <53297144+salty-stalin@users.noreply.github.com>
2020-04-08 00:25:41 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,10 +1,16 @@
 import requests
 import datetime
+
 now = datetime.date.today()
 print (now)
 yesterday = now - datetime.timedelta(days=1)
+print(yesterday)
 
-try:
+
+
+request = requests.get("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(now)+".xlsx")
+if request.status_code == 200:
+
 
     dls = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(now)+".xlsx"
     
@@ -16,11 +22,12 @@ try:
     output = open('COVID-19-geographic-disbtribution-worldwide.xlsx', 'wb')
     output.write(resp.content)
     output.close()
-except:
+    print("Todays data released")
+else:
     dls = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(yesterday)+".xlsx"
     
     
-    
+    print("Todays data not released yet downloading yesterdays data") 
     
     resp = requests.get(dls)
     
