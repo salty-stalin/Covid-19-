c1beb19150c60b57a59cde48dea4037874494473
salty-stalin <53297144+salty-stalin@users.noreply.github.com>
2020-04-08 00:19:06 +0100

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -2,16 +2,28 @@ import requests
 import datetime
 now = datetime.date.today()
 print (now)
+yesterday = now - datetime.timedelta(days=1)
 
+try:
 
-
-dls = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(now)+".xlsx"
-
-
-
-
-resp = requests.get(dls)
-
-output = open('COVID-19-geographic-disbtribution-worldwide.xlsx', 'wb')
-output.write(resp.content)
-output.close()
+    dls = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(now)+".xlsx"
+    
+    
+    
+    
+    resp = requests.get(dls)
+    
+    output = open('COVID-19-geographic-disbtribution-worldwide.xlsx', 'wb')
+    output.write(resp.content)
+    output.close()
+except:
+    dls = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-" +str(yesterday)+".xlsx"
+    
+    
+    
+    
+    resp = requests.get(dls)
+    
+    output = open('COVID-19-geographic-disbtribution-worldwide.xlsx', 'wb')
+    output.write(resp.content)
+    output.close()
